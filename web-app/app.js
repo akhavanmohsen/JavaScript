d611f6def4b6cb6e2c7bd22053cbc7a727c8f644
mohsenakhavan <akhavan.mohsen@gmail.com>
2020-07-24 14:15:31 +0300

    This is test file

--- web-app/app.js
+++ web-app/app.js
@@ -1,37 +1,34 @@
-const productList = [{
-    name : 'book1',
-    exist : true,
-},
-{
-    name : 'book2',
-    exist : false ,  
-},
-{
-    name : 'book3',
-    exist : true,
-},
-{
-    name : 'book4',
-    exist: false,
-}]
-let existCount = 0
-let counter = 0
+const products = []
 
-const availableProducts = productList.filter(function(item){
-    return item.exist === true
+const filters = {
+    searchItem: ''
+}
+
+const renderProducts = function(products, filters) {
+    const filteredProducts = products.filter(function(item) {
+        return item.title.toLowerCase().includes(filters.searchItem.toLowerCase())
+    })
+    document.querySelector('#products').innerHTML = ''
+    filteredProducts.forEach(function(item) {
+        const productEl = document.createElement('p')
+        productEl.textContent = item.title
+        document.querySelector('#products').appendChild(productEl)
+    })
+}
+
+renderProducts(products, filters)
+
+document.querySelector('#search-products').addEventListener('input', function(e) {
+    filters.searchItem = e.target.value
+    renderProducts(products, filters)
 })
-console.log(existCount)
 
-const existProducts = document.createElement('H2')
-existProducts.textContent = `The total of exist products is : ${availableProducts.length}`
-document.querySelector('body').appendChild(existProducts)
-
-const listOfProducts = document.createElement('P')
-listOfProducts.textContent = 'The below are list of product :'
-document.querySelector('body').appendChild(listOfProducts)
-productList.forEach(function(item){
-    counter = counter + 1
-    const eachProducts = document.createElement('p')
-    eachProducts.textContent = `${counter} - ${item.name}`
-    document.querySelector('body').appendChild(eachProducts)
+document.querySelector('#add-product-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    products.push({
+        title: e.target.elements.productTitle.value,
+        exists: true
+    })
+    renderProducts(products, filters)
+    e.target.elements.productTitle.value = ''
 })
